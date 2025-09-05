97c60d50e261f31205fa70015b191006bf02ad8f
AntoCoscia21 <cosciaantonio693@gmail.com>
2025-09-05 11:20:41 +0200

    Create definizioni.h

---
+++ definizioni.h
@@ -0,0 +1,17 @@
+#ifndef DEFINIZIONE_H
+#define DEFINIZIONE_H
+#define MAXNOME 30
+#define MAXTIPO 30
+#define DIM 100
+
+typedef struct {                       // andiamo a dichiarare una struct di nome cliente che comprende il numero il nome e il tipo di operazione da eseguire
+    int numero;
+    char nome[MAXNOME];
+    char tipoOperazione[MAXTIPO];
+} Cliente;
+
+typedef struct {                       //andiamo ad dichiarare una struct Coda la quale ci permette di inserire clienti alla coda rear, rimuovere cliente dalla testa front, tenere il conto dei clienti in coda count
+    Cliente clienti[DIM];
+    int front, rear, count;
+} Coda;
+
