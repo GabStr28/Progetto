9bbf4623b2e4f85fa3a345c87d3c25a7b51ac141
AntoCoscia21 <cosciaantonio693@gmail.com>
2025-09-06 11:58:42 +0200

    Create scelta.c

---
+++ scelta.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "definizione.h"
+
+void menuInput(int *scelta) {
+    printf("\n=== SPORTELLO POSTALE ===\n");
+    printf("0->Esci\n");
+    printf("1->Prendi numero\n");
+    printf("2->Servi cliente\n");
+    printf("3->Annulla ultimo (Undo)\n");
+    printf("4->Stampa storico clienti\n");
+    printf("5->Report ordinato e ricerca\n");
+    printf("Scelta: ");
+    scanf("%d", scelta);
+}
