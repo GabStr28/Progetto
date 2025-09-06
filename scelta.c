5cfc8952187ee080878f7ef6f40567f630fff3b9
GabStr28 <gabriele.28.10.04@gmail.com>
2025-09-06 14:42:38 +0200

    Update scelta.c

--- scelta.c
+++ scelta.c
@@ -4,7 +4,7 @@
 #include "definizione.h"
 
 void menu(int *scelta) {
-    printf("\n=== SPORTELLO POSTALE ===\n");
+    printf("\n---SPORTELLO POSTALE---\n");
     printf("0->Esci\n");
     printf("1->Prendi numero\n");
     printf("2->Servi cliente\n");
