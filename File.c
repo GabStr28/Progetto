54d70c6d91090c034f0cb614de7027e424b73a7d
GabStr28 <gabriele.28.10.04@gmail.com>
2025-09-07 09:10:32 +0200

    Update File.c

--- File.c
+++ File.c
@@ -4,19 +4,19 @@
 #include "definizione.h"
 
 void salvaClienteTesto(Cliente cl) {
-    FILE *f = fopen("serviti.txt", "a");     //apro un file di tipo txt di nome serviti
-    if (f == NULL) {                         //verifico se c'è qualche errore se è cosi comunico l'errore dell'apertura del file e torno indietro, altrimenti continuo 
+    FILE *f = fopen("serviti.txt", "a");                                  //apro un file di tipo txt di nome serviti
+    if (f == NULL) {                                                      //verifico se c'è qualche errore se è cosi comunico l'errore dell'apertura del file e torno indietro, altrimenti continuo 
                printf("Errore apertura file testo.\n"); 
                return; }
     fprintf(f, "%d,%s,%s\n", cl.numero, cl.nome, cl.tipoOperazione);      //scrico sul file numero nome e tipo di operazione sel cliente servito 
-    fclose(f);                                                          //chiudo il file
+    fclose(f);                                                            //chiudo il file
 }
 
 void salvaClienteBin(Cliente cl) {
-    FILE *f = fopen("serviti.bin", "ab");                         //apro file di tipo bin (binario)
+    FILE *f = fopen("serviti.bin", "ab");                                 //apro file di tipo bin (binario)
     if (f == NULL) { 
-                 printf("Errore apertura file binario.\n");        //come a prima verifico che non ci siano errori 
+                 printf("Errore apertura file binario.\n");               //come a prima verifico che non ci siano errori 
                 return; }
-    fwrite(&cl, sizeof(Cliente), 1, f);                        //scrivo: indirizzo della variabile cl, dimensione in byte della struct cliente, inseriamo la struct alla fine del file 
-    fclose(f);                                                  //chiudo il file 
+    fwrite(&cl, sizeof(Cliente), 1, f);                                   //scrivo: indirizzo della variabile cl, dimensione in byte della struct cliente, inseriamo la struct alla fine del file 
+    fclose(f);                                                            //chiudo il file 
 }
