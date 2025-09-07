197881969e6ac682a2479d9e20760a530915131a
GabStr28 <gabriele.28.10.04@gmail.com>
2025-09-07 09:12:12 +0200

    Update Lista.c

--- Lista.c
+++ Lista.c
@@ -3,15 +3,15 @@
 #include <string.h>
 #include "definizione.h"
 
-int inserisciInLista(Nodo **head, Cliente cl) {         //funzione che mi permette di inserire il cliente servito in lista 
+int inserisciInLista(Nodo **head, Cliente cl) {            //funzione che mi permette di inserire il cliente servito in lista 
     Nodo *nuovo = (Nodo *)malloc(sizeof(Nodo));            //malloc mi permette di avere nuova memoria 
     if (nuovo == NULL) {                                   //verifivo che la malloc non mi restituisca null ovvero se l'allocazione di memoria non è possibile 
         printf("Errore di memoria (lista).\n");
         return 0;
     }
-    nuovo->cl = cl;                     //nel nuovo nodo vado ad inserire il cliente servito
-    nuovo->next = *head;                //inserisco il nuovo nodo davanti alla vecchia testa 
-    *head = nuovo;                      //aggiorno la testa 
+    nuovo->cl = cl;                                        //nel nuovo nodo vado ad inserire il cliente servito
+    nuovo->next = *head;                                   //inserisco il nuovo nodo davanti alla vecchia testa 
+    *head = nuovo;                                         //aggiorno la testa 
     return 1;
 }
 
@@ -31,8 +31,8 @@ int rimuoviDaLista(Nodo **head, int numero) {
 }
 
 void stampaLista(Nodo *head) {
-    Nodo *p = head;           //dichiaro una variabile di tipo nodo e la punto alla testa della lista 
-    while (p != NULL) {       //stampo fino a quando non trovo lo spazio vuoto  
+    Nodo *p = head;                                       //dichiaro una variabile di tipo nodo e la punto alla testa della lista 
+    while (p != NULL) {                                   //stampo fino a quando non trovo lo spazio vuoto  
         printf("Num: %d - Nome: %s - Operazione: %s\n",
                p->cl.numero, p->cl.nome, p->cl.tipoOperazione);
         p = p->next;   //passo a quello successivo
